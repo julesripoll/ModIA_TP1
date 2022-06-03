dce6ffc41da18ce639bed28d9583e7df5dadd9b0
Jules Ripoll <jules.ripoll@gmail.com>
2022-06-03 11:30:22 +0200

    tp fini

--- mnist_net.py
+++ mnist_net.py
@@ -8,24 +8,31 @@ import torch.nn.functional as F
 
 class MNISTNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(...)
-        self.conv2 = nn.Conv2d(...)
-        self.pool = nn.MaxPool2d(...)
-        self.fc1 = nn.Linear(...)
-        self.fc2 = nn.Linear(...)
-        self.fc3 = nn.Linear(...)
+        super(MNISTNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=8,kernel_size=(5,5))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2)) #on va faire un max pooling sur une fenêtre de taille 5x5
+        self.conv2 = nn.Conv2d(in_channels=8,out_channels=16, kernel_size=(5,5))
+        self.flat=nn.Flatten()
+        self.fc1 = nn.Linear(in_features=256,out_features=128)
+        self.fc2 = nn.Linear(in_features=128,out_features=64)
+        self.fc3 = nn.Linear(in_features=64,out_features=10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))       # First convolution followed by
         x = self.pool(x)                # a relu activation and a max pooling#
-        x = ...
-        ...
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)                # a relu activation and a max pooling#
+        x=self.flat(x)
+        x=F.relu(self.fc1(x))
+        x=F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
     def get_features(self, x):
-        pass
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        return x
 
 
 if __name__=='__main__':
--- train_mnist.py
+++ train_mnist.py
@@ -14,7 +14,7 @@ from mnist_net import MNISTNet
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train(net, optimizer, loader, epochs=10):
+def train(net, optimizer, loader, writer, epochs=10):
     criterion = nn.CrossEntropyLoss()
     for epoch in range(epochs):
         running_loss = []
@@ -28,6 +28,7 @@ def train(net, optimizer, loader, epochs=10):
             loss.backward()
             optimizer.step()
             t.set_description(f'training loss: {mean(running_loss)}')
+        writer.add_scalar('training loss', mean(running_loss), epoch)
 
 def test(model, dataloader):
     test_corrects = 0
@@ -43,18 +44,20 @@ def test(model, dataloader):
 
 if __name__=='__main__':
 
+  writer = SummaryWriter(f'runs/MNIST')
+
   parser = argparse.ArgumentParser()
   
   parser.add_argument('--exp_name', type=str, default = 'MNIST', help='experiment name')
-  parser.add_argument(...)
-  parser.add_argument(...)
-  parser.add_argument(...)
+  parser.add_argument('--epochs', type=int,default=3,help='nombre epochs')
+  parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+  parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 
   args = parser.parse_args()
   exp_name = args.exp_name
-  epochs = ...
-  batch_size = ...
-  lr = ...
+  epochs = args.epochs
+  batch_size = args.batch_size
+  lr = args.lr
 
   # transforms
   transform = transforms.Compose(
@@ -69,12 +72,28 @@ if __name__=='__main__':
   trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
   testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
   
-  net = ...
+  net = MNISTNet()
   # setting net on device(GPU if available, else CPU)
   net = net.to(device)
-  optimizer = optim.SGD(...)
+  optimizer = optim.SGD(params=net.parameters(),lr=lr, momentum=0.9)
 
-  train(...)
-  test_acc = test(...)
+  train(net=net, optimizer=optimizer,loader=trainloader,writer=writer, epochs=epochs)
+  test_acc = test(model=net,dataloader=testloader)
   print(f'Test accuracy:{test_acc}')
   torch.save(net.state_dict(), "mnist_net.pth")
+
+  #add embeddings to tensorboard
+  perm = torch.randperm(len(trainset.data))
+  images, labels = trainset.data[perm][:256], trainset.targets[perm][:256]
+  images = images.unsqueeze(1).float().to(device)
+  with torch.no_grad():
+    embeddings = net.get_features(images)
+    writer.add_embedding(embeddings,
+                  metadata=labels,
+                  label_img=images, global_step=1)
+
+  # save networks computational graph in tensorboard
+  writer.add_graph(net, images)
+  # save a dataset sample in tensorboard
+  img_grid = torchvision.utils.make_grid(images[:64])
+  writer.add_image('mnist_images', img_grid)
